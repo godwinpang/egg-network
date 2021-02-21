42662e906d479c633ce7e2d15b1aee49904cb1e9
gypang <gypang24@gmail.com>
2021-02-21 09:44:37 -0800

    push

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src="https://i.pinimg.com/736x/87/b4/98/87b498f5af1483c727075990fcd7f15b.jpg"
+          className="App-logo"
+          alt="logo"
+        />
+        <h1>Egg Network :)</h1>
       </header>
     </div>
   );
